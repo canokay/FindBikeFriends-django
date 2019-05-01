21361fa3f516a99ce0b3645e0819d7dd11cebde8
Can Okay <canokay2014@gmail.com>
2019-05-01 19:07:31 +0300

    Postgresql config added.

--- FindBikeFriends/settings.py
+++ FindBikeFriends/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'FindBikeFriends.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'findbikefriends19db',
+        'USER': 'fbfuser',
+        'PASSWORD': 'fbfuser19',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
