2fc6e966b3f63ff106ac15884ebb07d116bef95e
Can Okay <canokay2014@gmail.com>
2019-05-13 13:50:49 +0300

    bi şeyler oldu

--- FindBikeFriends_web/urls.py
+++ FindBikeFriends_web/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 from FindBikeFriends_web.views import IndexView, LoginView, RegisterView
 
@@ -9,3 +12,4 @@ urlpatterns = [
     url(r'^login/$', LoginView, name='login'),
     url(r'^register/$', RegisterView, name='register'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
